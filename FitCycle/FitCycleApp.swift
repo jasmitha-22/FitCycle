b7f775749961c73e478f584c986ce24a869f681c
jasmitha-22 <gourabathinijasmitha@gmail.com>
2024-06-24 12:41:30 -0600

    Initial Commit

---
+++ FitCycle/FitCycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitCycleApp.swift
+//  FitCycle
+//
+//  Created by Jasmitha Gourabathini on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct FitCycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
