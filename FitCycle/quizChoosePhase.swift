99835a5f2a8cdfd51293db065fd97b837c02bfa9
Zahra <zahramoosanni@icloud.com>
2024-06-25 12:24:58 -0500

    making sure everything is in here

--- FitCycle/quizChoosePhase.swift
+++ FitCycle/quizChoosePhase.swift
@@ -9,14 +9,32 @@ import SwiftUI
 
 struct quizChoosePhase: View {
     var body: some View {
-        ZStack{
-            Color(red: 1, green: 0.8705882352941177, blue: 0.8705882352941177)
-                .ignoresSafeArea()
-            VStack{
-               //just some ideas...totally up for change
-                Text("Quiz??")
-                Text("When was the first day of the menstrual cycle?")
-                Text("How are your energy levels today?")
+        NavigationStack{
+            ZStack{
+                Color(red: 1, green: 0.8705882352941177, blue: 0.8705882352941177)
+                    .ignoresSafeArea()
+                VStack{
+                    //just some ideas...totally up for change
+                    Text("What phase of your cycle are you in?")
+                        .padding()
+                    NavigationLink(destination: menstrual_()) {
+                        Text ("Days 1-5: Menstrual")
+                            .padding()
+                    }
+                    NavigationLink(destination: follicular_()) {
+                        Text ("Days 5-10: Follicular")
+                            .padding()
+                    }
+                    NavigationLink(destination: luteal()) {
+                        Text ("Days 10-15: Luteal")
+                            .padding()
+                    }
+                    NavigationLink(destination: ovulatory()) {
+                        Text ("Days 15-20: Ovulatory")
+                            .padding()
+                    }
+                    }
+                
 
             }
         }
