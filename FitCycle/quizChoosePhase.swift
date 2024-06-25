397273ff302450a7f8db2e3f96c0642390cd01da
jasmitha-22 <gourabathinijasmitha@gmail.com>
2024-06-25 11:44:23 -0600

    changes to buttons

--- FitCycle/quizChoosePhase.swift
+++ FitCycle/quizChoosePhase.swift
@@ -13,7 +13,7 @@ struct quizChoosePhase: View {
             ZStack{
                 Color(red: 1, green: 0.8705882352941177, blue: 0.8705882352941177)
                     .ignoresSafeArea()
-                VStack{
+                VStack(spacing: 50){
                     //just some ideas...totally up for change
                     Text("What phase of your cycle are you in?")
                         .padding()
@@ -21,20 +21,43 @@ struct quizChoosePhase: View {
                         Text ("Days 1-5: Menstrual")
                             .padding()
                     }
+                    .buttonStyle(.borderedProminent)
+                    .buttonBorderShape(.roundedRectangle(radius: 10))
+                    .tint(.init(red: 0.39215686274509803 ,green: 0.3137254901960784, blue: 0.40784313725490196))
+                    .font(.callout)
+                    .foregroundColor(Color(red: 1, green: 0.8705882352941177, blue: 0.8705882352941177))
+                    
                     NavigationLink(destination: follicular_()) {
                         Text ("Days 5-10: Follicular")
                             .padding()
                     }
+                    .buttonStyle(.borderedProminent)
+                    .buttonBorderShape(.roundedRectangle(radius: 10))
+                    .tint(.init(red: 0.39215686274509803 ,green: 0.3137254901960784, blue: 0.40784313725490196))
+                    .font(.callout)
+                    .foregroundColor(Color(red: 1, green: 0.8705882352941177, blue: 0.8705882352941177))
+                    
                     NavigationLink(destination: luteal()) {
                         Text ("Days 10-15: Luteal")
                             .padding()
                     }
+                    .buttonStyle(.borderedProminent)
+                    .buttonBorderShape(.roundedRectangle(radius: 10))
+                    .tint(.init(red: 0.39215686274509803 ,green: 0.3137254901960784, blue: 0.40784313725490196))
+                    .font(.callout)
+                    .foregroundColor(Color(red: 1, green: 0.8705882352941177, blue: 0.8705882352941177))
+                    
                     NavigationLink(destination: ovulatory()) {
                         Text ("Days 15-20: Ovulatory")
                             .padding()
                     }
+                    .buttonStyle(.borderedProminent)
+                    .buttonBorderShape(.roundedRectangle(radius: 10))
+                    .tint(.init(red: 0.39215686274509803 ,green: 0.3137254901960784, blue: 0.40784313725490196))
+                    .font(.callout)
+                    .foregroundColor(Color(red: 1, green: 0.8705882352941177, blue: 0.8705882352941177))
+
                     }
-                
 
             }
         }
