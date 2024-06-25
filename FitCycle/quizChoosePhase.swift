03a17323f5eb36ef15e761661066cb1b5b02a99c
jasmitha-22 <gourabathinijasmitha@gmail.com>
2024-06-24 22:58:35 -0600

    built the home page and a navigation to a new page.

---
+++ FitCycle/quizChoosePhase.swift
@@ -0,0 +1,18 @@
+//
+//  quizChoosePhase.swift
+//  FitCycle
+//
+//  Created by Jasmitha Gourabathini on 6/24/24.
+//
+
+import SwiftUI
+
+struct quizChoosePhase: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    quizChoosePhase()
+}
