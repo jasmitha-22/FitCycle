9e2c9dcf9c078c4bf61c9d340e8825b933c13c62
tamubasvi <tamubasvi@gmail.com>
2024-06-26 13:09:20 -0400

    Ovu and Lutral

--- FitCycle/luteal.swift
+++ FitCycle/luteal.swift
@@ -4,15 +4,44 @@
 //
 //  Created by Zahra Moosani on 6/25/24.
 //
-
 import SwiftUI
-
+import WebKit
 struct luteal: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        
+        ZStack { Color(red: 1, green: 0.8705882352941177, blue: 0.8705882352941177)
+                .ignoresSafeArea()
+           
+          
+            VStack { Text("Luteal")
+                    .font(.title)
+                    .padding(.all, 37.0)
+                Spacer()
+                Text("Recomended workout...")
+                    .font(.headline)
+                        .padding(.all, 37.0)
+                    Spacer()
+                    
+                Text(" Lasts 12-14 days, characterized by fatigue, groggness, enflamation. ")
+                    .font(.headline)
+                    .padding(.all, 30)
+                    .lineLimit(5)
+                
+                
+                
+            
+                
+                
+            }
+        }
     }
-}
-
+    
+   
+    }
 #Preview {
     luteal()
 }
+
+
--- FitCycle/ovulatory.swift
+++ FitCycle/ovulatory.swift
@@ -1,19 +1,37 @@
+
 //
 //  ovulatory.swift
 //  FitCycle
 //
 //  Created by Zahra Moosani on 6/25/24.
 //
-
 import SwiftUI
-
 struct ovulatory: View {
     var body: some View {
-        Text("This is intresting")
-        Text("Tamutenda made change")
+        
+       
+        ZStack { Color(red: 1, green: 0.8705882352941177, blue: 0.8705882352941177)
+                .ignoresSafeArea()
+            
+            VStack { Text("Ovulatory")
+                    .font(.title)
+                    .padding(.all, 37.0)
+                Spacer()
+                
+                Text("Lasts 12-24 hours, signs of ovulation include more slippery, wet cervical mucus.")
+                    .font(.headline)
+                    .padding(.all, 30)
+                    .lineLimit(5)
+                
+                
+                
+            }
+            
+        }
     }
 }
-
 #Preview {
     ovulatory()
 }
+
+
